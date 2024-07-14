544e0ce8962b07e69477b377578840552ec6224e
Kevin <kemalsogut7c@gmail.com>
2024-07-14 18:08:43 -0400

    woman

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Come on mannn!</Text>
+      <Text>Come on WOmannn!</Text>
       <StatusBar style="auto" />
     </View>
   );
