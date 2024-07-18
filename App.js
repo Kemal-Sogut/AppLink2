7dea0626b92846c560f09dbeb2354c1a76c43e85
Kevin <kemalsogut7c@gmail.com>
2024-07-17 21:41:16 -0400

    testtt

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Madrid!
-      </Text>
+      <Text>Hello Madrid!</Text><Text>Hello Madrid!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
