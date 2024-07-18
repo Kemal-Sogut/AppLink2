3988c86b8b739bd7bf7862e7ce12ea6bc5aa5f99
Kevin <kemalsogut7c@gmail.com>
2024-07-17 21:05:00 -0400

    second line

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Come on WOmannn!</Text>
+      <Text>Come on Maaan</Text>
+      <Text>Come on Maaan</Text>
       <StatusBar style="auto" />
     </View>
   );
